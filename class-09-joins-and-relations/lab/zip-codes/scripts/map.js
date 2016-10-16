b8a2c84ca947c0761afc5fd07658041255b55fce
Will Skelton <william.f.skelton@outlook.com>
2016-10-16 16:18:04 -0700

    Just one TOD left

--- class-09-joins-and-relations/lab/zip-codes/scripts/map.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/map.js
@@ -1,15 +1,31 @@
-function initMap() {
+function initMap(latitude, longitude, mapTitle) {
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 47.611435, lng: -122.330456},
+    center: {lat: latitude, lng: longitude},
     scrollwheel: true,
     zoom: 8
   });
 
-  // TODO: Follow the Google Maps API docs to create markers on the map based on the search options on the home page.
+  // // TODO: Follow the Google Maps API docs to create markers on the map based on the search options on the home page.
+  // var latAndLong = new google.maps.LatLng(latitude, longitude);
+  // var mapOptions = {
+  //   zoom: 4,
+  //   center: latAndLong
+  // }
+  // var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  //
   // var marker = new google.maps.Marker({
-  //   position: {lat: 47.611435, lng: -122.330456},
-  //   map: map,
-  //   title: 'Hello World!'
+  //     position: latAndLong,
+  //     title: mapTitle
   // });
+  //
+  // // To add the marker to the map, call setMap();
+  // marker.setMap(map);
+
+  // var marker = new google.maps.Marker({
+  //   position: {lat: latitude, lng: longitude},
+  //   map: map,
+  //   title: mapTitle
+  // });
+
 }
--- class-09-joins-and-relations/lab/zip-codes/scripts/search.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/search.js
@@ -5,32 +5,44 @@
   var citySelector = $('#city-select');
   var allStates = [];
   function populateStateFilter() {
-    webDB.execute('SELECT DISTINCT state FROM zips', function(result){
+    webDB.execute('SELECT DISTINCT state FROM zips ORDER BY state', function(result){
       result.forEach(function(currentValue){
         stateSelector.append('<option class="stateOption" value=' + currentValue.state + '>' + currentValue.state +'</option>')
       })
     })
   }
 
-function populateCityFilter() {
-  var selectedState = this.value;
-  console.log("=========================");
-  console.log(selectedState);
-  webDB.execute([{
-    'sql': 'SELECT DISTINCT city FROM zips WHERE state=?',
-    'data': [selectedState]}], function(result) {
-      result.forEach(function(currentValue){
-        citySelector.append('<option class="cityOption" value=' + currentValue.city + '>' + currentValue.city +'</option>')
-      })
-    }
-  )
-}
+  function populateCityFilter() {
+    var selectedState = this.value;
+    console.log("=========================");
+    console.log(selectedState);
+    webDB.execute([{
+      'sql': 'SELECT DISTINCT city FROM zips WHERE state=?',
+      'data': [selectedState]}], function(result) {
+        result.forEach(function(currentValue){
+          citySelector.append('<option class="cityOption" value=' + currentValue.city + '>' + currentValue.city +'</option>')
+        })
+      }
+    )
+  }
 
+  function zipSearch(event){
+    event.preventDefault();
+    var zipCode = $('#input').val();
+    webDB.execute('SELECT * FROM zips WHERE zip=' + zipCode, function(result) {
+      var lat = result.latitude;
+      var long = result.longitude;
+      var name = result.city;
+      initMap(lat, long, name);
+      console.log(result);
+    })
+  }
 
-populateStateFilter();
-stateSelector.on('change', populateCityFilter);
+  populateStateFilter();
+  stateSelector.on('change', populateCityFilter);
 
-  // TODO: You will also interact with the map.js file here
-
+  // TODOID: You will also interact with the map.js file here
+  var form = $('form');
+  form.on('submit', zipSearch);
 
 })(window)
