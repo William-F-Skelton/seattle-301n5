4eb209ad6b29ae5e54884973972194be3745b2ab
Will Skelton <william.f.skelton@outlook.com>
2016-10-16 15:12:19 -0700

    3/4 TODOS are TODID

--- class-09-joins-and-relations/lab/zip-codes/scripts/search.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/search.js
@@ -1,7 +1,34 @@
 (function(module) {
 
-  // TODO: Write the code to populate your filters, and enable the search queries here in search.js
-  
+  // TODID: Write the code to populate your filters, and enable the search queries here in search.js
+  var stateSelector = $('#state-select');
+  var citySelector = $('#city-select');
+  var allStates = [];
+  function populateStateFilter() {
+    webDB.execute('SELECT DISTINCT state FROM zips', function(result){
+      result.forEach(function(currentValue){
+        stateSelector.append('<option class="stateOption" value=' + currentValue.state + '>' + currentValue.state +'</option>')
+      })
+    })
+  }
+
+function populateCityFilter() {
+  var selectedState = this.value;
+  console.log("=========================");
+  console.log(selectedState);
+  webDB.execute([{
+    'sql': 'SELECT DISTINCT city FROM zips WHERE state=?',
+    'data': [selectedState]}], function(result) {
+      result.forEach(function(currentValue){
+        citySelector.append('<option class="cityOption" value=' + currentValue.city + '>' + currentValue.city +'</option>')
+      })
+    }
+  )
+}
+
+
+populateStateFilter();
+stateSelector.on('change', populateCityFilter);
 
   // TODO: You will also interact with the map.js file here
 
