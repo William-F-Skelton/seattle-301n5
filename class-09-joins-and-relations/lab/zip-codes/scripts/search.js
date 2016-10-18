cce0bd9322e351c718e5acab29d9e8ea3ab0f26a
Will Skelton <william.f.skelton@outlook.com>
2016-10-17 21:18:08 -0700

    city-state search is plotting but plotting wrong markers.

--- class-09-joins-and-relations/lab/zip-codes/scripts/search.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/search.js
@@ -14,17 +14,46 @@
 
   function populateCityFilter() {
     var selectedState = this.value;
+    var args = [];
     console.log("=========================");
-    console.log(selectedState);
+    console.log('selectedState: ' + selectedState);
+    console.log("=========================");
     webDB.execute([{
-      'sql': 'SELECT DISTINCT city FROM zips WHERE state=?',
+      'sql': 'SELECT DISTINCT city FROM zips  WHERE state=? ORDER BY city',
       'data': [selectedState]}], function(result) {
+        console.log("=========================");
+        console.log('First Result: ');
+        console.log(result[0]);
+        console.log("=========================");
+        citySelector.empty();
+        citySelector.append('<option value="city">Select a City</option>')
         result.forEach(function(currentValue){
-          citySelector.append('<option class="cityOption" value=' + currentValue.city + '>' + currentValue.city +'</option>')
+          var cityString = currentValue.city + '';
+          citySelector.append('<option class="cityOption" value=' + cityString.replace(/ /g, "") + '>' + cityString.toLowerCase() +'</option>');
+          // console.log(cityString.replace(/ /g, ""));
         })
       }
     )
+    citySelector.on('change', function(){
+      var selectedCity = this.value;
+      // console.log('1: ' + this.value);
+      webDB.execute([{
+        'sql': 'SELECT DISTINCT latitude, longitude, city FROM zips WHERE city=?',
+        'data': [selectedCity]}], function(result) {
+          console.log(result);
+          // console.log('args:');
+          // console.log(result);
+          args = result;
+          // console.log(args);
+          // console.log(args[0].latitude);
+          // console.log(args[0].longitude);
+          // console.log(args[0].city);
+          plotMarkers(args);
+        }
+      )
+    })
   }
+
 
   function zipSearch(event){
     event.preventDefault();
@@ -33,13 +62,15 @@
       var lat = result.latitude;
       var long = result.longitude;
       var name = result.city;
-      initMap(lat, long, name);
+      plotMarkers([lat, long, name]);
       console.log(result);
     })
   }
 
   populateStateFilter();
   stateSelector.on('change', populateCityFilter);
+
+
 
   // TODOID: You will also interact with the map.js file here
   var form = $('form');
